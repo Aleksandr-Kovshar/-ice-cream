6c8561cf5b8e43e6ec0abac5dffeec654c055060
Nikolay-Akymenko <103488690+Nikolay-Akymenko@users.noreply.github.com>
2022-06-25 14:09:27 +0000

    Deploying to gh-pages from @ Nikolay-Akymenko/ice-cream@715054874785c4dbe3c492642975d338d1d051e2 🚀

--- index.13c67bd1.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0,speed:1e3});
-//# sourceMappingURL=index.13c67bd1.js.map
--- index.19599b37.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0,speed:1e3});
-//# sourceMappingURL=index.19599b37.js.map
