dc253ae0d55dc2de602438105c4b99f1580d3877
Nikolay-Akymenko <103488690+Nikolay-Akymenko@users.noreply.github.com>
2022-06-28 18:06:48 +0000

    Deploying to gh-pages from @ Nikolay-Akymenko/ice-cream@82d42d5f083fd0ae086545b352d51658abb74ce4 🚀

--- index.53e055e1.js
+++
@@ -1,2 +0,0 @@
-var offset=100,scrollUp=document.querySelector(".button-up"),getTop=function(){return window.pageYOffset};window.addEventListener("scroll",(function(){getTop()>offset?scrollUp.classList.add("button-up--visible"):scrollUp.classList.remove("button-up--visible")})),scrollUp.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
-//# sourceMappingURL=index.53e055e1.js.map
